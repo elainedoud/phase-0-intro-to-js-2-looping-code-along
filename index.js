aacadf544e3c7490d5cbaa0aab77da4fd062e468
Elaine <elainedoud@gmail.com>
2022-10-06 11:24:04 -0400

    commit tree e8c3e6798121de2b3357520497d1d4e7e53be529

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+
+function writeCards(names, celebration) {
+    let messages = [];
+    let i = 0;
+    for (i; i < names.length; i++) {
+        messages === messages.push(`Thank you, ${names[i]}, for the wonderful ${celebration} gift!`);
+    }
+    return messages;
+}
+
+console.log(messages);
+
+/*function countDown(i) {
+    let numbers =[];
+    while (i > 0) {
+        numbers === numbers.push(`${i}`);
+        i -= 1;
+    }
+}
+
+console.log(numbers());*/
+
+ function countDown(i) {
+    while (i > -1) {
+        console.log(i);
+        i--;
+    };
+    return i;
+}
+countDown();
+    
